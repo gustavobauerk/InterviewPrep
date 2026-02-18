59a7cd8c079dcb648bca58971fa5b0e1e79e2851
gustavoknapp <gustavo.bauer.knapp@gmail.com>
2026-02-18 08:26:13 +0000

    zigzag

--- main/TreeBFS/TreeBFS.java
+++ main/TreeBFS/TreeBFS.java
@@ -10,10 +10,39 @@ public class TreeBFS {
         root.left.left = new TreeNode(9);
         root.right.left = new TreeNode(10);
         root.right.right = new TreeNode(5);
-        System.out.println(traverse(root));
+        System.out.println(zigzag(root));
     }
 
-    private static List<List<Integer>> traverse(TreeNode root) {
+    private static List<List<Integer>> zigzag(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        boolean zig = true;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                if(zig) {
+                    list.add(poll.val);
+                }else  {
+                    list.add(0, poll.val);
+                }
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+            zig = !zig;
+            result.add(list);
+        }
+        return result;
+    }
+
+
+    private static List<List<Integer>> reverse(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.add(root);
