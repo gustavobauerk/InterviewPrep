94e77e079de554b9c6c284135e9e4b27483f2772
gustavoknapp <gustavo.bauer.knapp@gmail.com>
2026-02-23 08:08:32 +0000

    find maximum path sum

--- main/TreeDFS/TreeDFSSum.java
+++ main/TreeDFS/TreeDFSSum.java
@@ -52,6 +52,28 @@ public class TreeDFSSum {
         int helperRight = helper(root.right, value);
         return helperLeft + helperRight;
     }
+    private static int globalMaximumSum= Integer.MIN_VALUE;
+
+    private static int findMaximumPathSum(TreeNode root) {
+        globalMaximumSum= Integer.MIN_VALUE;
+        int sum = findMaximumPathSumHelper(root);
+        return Math.max(sum, globalMaximumSum);
+    }
+
+    private static int findMaximumPathSumHelper(TreeNode root){
+        if(root == null) return -1001;
+        if(root.left == null && root.right == null) {
+            return root.val;
+        }
+        int leftPath = findMaximumPathSumHelper(root.left);
+        int rightPath = findMaximumPathSumHelper(root.right);
+        globalMaximumSum = Math.max(globalMaximumSum, leftPath);
+        globalMaximumSum = Math.max(globalMaximumSum, rightPath);
+        globalMaximumSum = Math.max(globalMaximumSum, root.val);
+        globalMaximumSum = Math.max(globalMaximumSum, leftPath + rightPath + root.val);
+
+        return Math.max(Math.max(leftPath, rightPath) + root.val, root.val);
+    }
 
 
     public static void main(String[] args) {
@@ -61,7 +83,11 @@ public class TreeDFSSum {
         treeNode.left.left = new TreeNode(1);
         treeNode.right.left = new TreeNode(6);
         treeNode.right.right = new TreeNode(5);
-        System.out.println(hasPath(treeNode, 8));
-        System.out.println(findSumOfPathNumbers(treeNode));
+//        System.out.println(hasPath(treeNode, 8));
+//        System.out.println(findSumOfPathNumbers(treeNode));
+        treeNode = new TreeNode(-1);
+        treeNode.left = new TreeNode(-3);
+
+        System.out.println(findMaximumPathSum(treeNode));
     }
 }
